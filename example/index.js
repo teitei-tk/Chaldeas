f50c7150fa82a3b4cc2544ee4334f2933c72083d
Yoshikatsu Higa <teitei.tk@gmail.com>
2017-06-07 13:41:20 +0900

    update example

--- example/index.js
+++ example/index.js
@@ -1,3 +1,6 @@
+// captureScreenshot
+// from : https://chromedevtools.github.io/devtools-protocol/tot/Page/#method-captureScreenshot
+const fs = require('fs');
 const chaldeas = require('./../src');
 
 const Chaldeas = chaldeas.default;
@@ -6,13 +9,16 @@ const c = Chaldeas.new();
 c.fetchProtocol().then((r) => {
   Promise.all([
     r.Network.enable(),
-    r.DOM.enable(),
+    r.Page.enable(),
   ]).then(() => {
-    const a = r.DOM.getDocument();
-    a.then((b) => {
-      console.log(b);
+    r.Page.navigate({ url: 'https://github.com/teitei-tk' });
+    r.Page.captureScreenshot().then((cap) => {
+      const fr = Buffer.from(cap.data, 'base64');
+      fs.writeFile('foo.png', fr, (err) => {
+        console.log(err);
+      });
+    }).then(() => {
+      c.terminate();
     });
-  }).then(() => {
-    c.terminate();
   });
 });
